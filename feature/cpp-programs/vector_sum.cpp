7b36a3cd9ce15ce55e8a9ad727c52e30628663a7
Saad Naseer <saadnaseer146@gmail.com>
2026-05-20 18:40:31 +0500

    Isolate Java samples in feature/java-programs

--- feature/cpp-programs/vector_sum.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-int main() {
-    std::vector<int> v{1,2,3,4,5};
-    int s = 0;
-    for (int x : v) s += x;
-    std::cout << "Sum: " << s << std::endl;
-    return 0;
-}
